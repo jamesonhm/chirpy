61285e34ab0d0fdbdab918934b2ec6a5c4ac61dc
jamesonhm <jamesonhmarshall@gmail.com>
2024-11-15 05:34:14 -0700

    complete ch1-5 fileserver

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 
 func main() {
 	const port = ":8080"
+	const filepathRoot = "."
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := &http.Server{
 		Addr:    port,
 		Handler: mux,
 	}
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 
