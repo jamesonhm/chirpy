c7c8e9b21c2c67034813d6f06df367a5a7246360
jamesonhm <jamesonhmarshall@gmail.com>
2024-11-16 18:11:08 -0700

    work on ch4-2 json

--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	//"fmt"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	serverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.serverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	htmlString := fmt.Sprintf(`
+<html>
+    <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited %d times!</p>
+    </body>
+</html>
+`, cfg.serverHits.Load())
+	w.Write([]byte(htmlString))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.serverHits.Swap(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func main() {
 	const port = ":8080"
 	const filepathRoot = "."
 
+	cfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", healthHandler)
+
+	mux.Handle("/app/", cfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("POST /api/validate_chirp", chirpValidHandler)
+
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 
 	srv := &http.Server{
 		Addr:    port,
@@ -27,4 +69,39 @@ func healthHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func chirpValidHandler(w http.ResponseWriter, req *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+	type errVal struct {
+		Err string `json:"error"`
+	}
+	type retVal struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	c := chirp{}
+	err := decoder.Decode(&c)
+	if err != nil {
+		log.Printf("Error decoding chirp: %s", err)
+		respBody := errVal{
+			Err: "error decoding chirp",
+		}
+		dat, _ := json.Marshal(respBody)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write(dat)
+		return
+	}
+	respBody := retVal{
+		Valid: true,
+	}
+	dat, _ := json.Marshal(respBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+
 }
